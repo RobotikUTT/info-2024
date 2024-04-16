a3ca7a965fcaea1afc00e17802f253bf6a520bcf
Teddy Roncin <teddy.roncin@proton.me>
2024-04-17 01:46:08 +0200

    added DetectionService

--- lidar/prog.py
+++ lidar/prog.py
@@ -1,5 +1,11 @@
+from collections.abc import Callable
+from math import acos, inf, pi, sin, sqrt, cos
 from threading import Thread, RLock
+from typing import Iterable, List, Mapping, Tuple
 from serial import Serial
+import time
+import numpy as np
+from scipy.optimize import minimize
 
 PACKET_SIZE = 47
 CRC_TABLE = b"\x00\x4d\x9a\xd7\x79\x34\xe3\xae\xf2\xbf\x68\x25\x8b\xc6\x11\x5c" \
@@ -18,53 +24,177 @@ CRC_TABLE = b"\x00\x4d\x9a\xd7\x79\x34\xe3\xae\xf2\xbf\x68\x25\x8b\xc6\x11\x5c" 
             b"\xeb\xa6\x71\x3c\x92\xdf\x08\x45\x19\x54\x83\xce\x60\x2d\xfa\xb7" \
             b"\x5d\x10\xc7\x8a\x24\x69\xbe\xf3\xaf\xe2\x35\x78\xd6\x9b\x4c\x01" \
             b"\xf4\xb9\x6e\x23\x8d\xc0\x17\x5a\x06\x4b\x9c\xd1\x7f\x32\xe5\xa8"
+DELETE_POINTS_TIMEOUT = 1
+
+class PositionService:
+    def get_position(self):
+        return (0, 0)
+    def get_angle(self):
+        return 0
+
+class PointData:
+    def __init__(self, angle, distance, robot_position, robot_angle, measured_at):
+        super.__setattr__("angle", angle)
+        super.__setattr__("distance", distance)
+        super.__setattr__("robot_position", robot_position)
+        super.__setattr__("robot_angle", robot_angle)
+        super.__setattr__("x", cos(robot_angle + angle) * distance + robot_position[0])
+        super.__setattr__("y", sin(robot_angle + angle) * distance + robot_position[1])
+        super.__setattr__("absolute_angle", (robot_angle + angle) % (2 * pi))
+        super.__setattr__("measured_at", measured_at)
+    def __setattr__(self, name: str, value: time.Any) -> None:
+        raise TypeError("PointData is immutable")
 
 class LidarService(Thread):
-
-    def __init__(self):
+    def __init__(self, position_service):
         super().__init__()
         self.serial = Serial("/dev/ttyS0", baudrate = 230400)
-        self.values = {}
+        self.values: List[PointData] = []
         self.lock = RLock()
-        
+        self.position_service = position_service
 
     def run(self):
         while True:
-            i = 0
-            while True :
-                j = 0
-                while self.serial.read() != b"T" and self.serial.read() != b",":
-                    j+=1
-                packet = bytearray(b"\x54\x2C")
-                while len(packet) < PACKET_SIZE:
-                    packet.extend(self.serial.read())
-                start_angle = stick_bytes(packet[4:6]) / 100
-                end_angle = stick_bytes(packet[42:44]) / 100
-                data = packet [6:42]
-                expected_crc = packet[-1]
-                # CRC check
-                crc = 0
-                for b in packet[:-1]:
-                   crc = CRC_TABLE[(crc ^ b) & 0xff]
-                if crc != expected_crc:
-                    print(crc, expected_crc)
-                    continue
-                with self.lock:
-                    angle_step = (end_angle - start_angle) / 11
-                    for i in range(12):
-                        angle = (start_angle + angle_step * i) % 360
-                        self.values[angle] = (data[i * 3 + 1] << 8) | data[i * 3]
+            j = 0
+            while self.serial.read() != b"T" and self.serial.read() != b",":
+                j+=1
+            packet = bytearray(b"\x54\x2C")
+            now = time.time()
+            while len(packet) < PACKET_SIZE:
+                packet.extend(self.serial.read())
+            start_angle = stick_bytes(packet[4:6]) / 100
+            end_angle = stick_bytes(packet[42:44]) / 100
+            while end_angle < start_angle:
+                end_angle += 360
+            data = packet [6:42]
+            expected_crc = packet[-1]
+            # CRC check
+            crc = 0
+            for b in packet[:-1]:
+                crc = CRC_TABLE[(crc ^ b) & 0xff]
+            if crc != expected_crc:
+                print(crc, expected_crc)
+                continue
+            with self.lock:
+                angle_step = (end_angle - start_angle) / 11
+                robot_position = self.position_service.get_position()
+                robot_angle = self.position_service.get_angle()
+                for i in range(12):
+                    angle = ((start_angle + angle_step * i) % 360) / 360 * 2 * pi
+                    distance = stick_bytes(data[i * 3:i * 3 + 1])
+                    self.values.append(PointData(angle, distance, robot_position, robot_angle, now))
 
     def get_values(self):
+        delete_before = time.time() - DELETE_POINTS_TIMEOUT
         with self.lock:
-            return {**self.values}
-            
+            self.values = dict(filter(lambda entry: entry[1][1] < delete_before, self.values))
+            return [*self.values]
+
+class ObjectData:
+    def __init__(self):
+        self.last_detected_at = time.time()
+        self.points: List[PointData] = []
+        self.boundary_points: Tuple[PointData, PointData] = []
+        self.circle_center = (0, 0)
+        self.circle_radius = 0
+        self.separations_to_make = []
+
+    def commit(self):
+        self.circle = fit_circle_to_points([point.x, point.y] for point in self.points)
+        new_splits = []
+        for i, line_coefficients in enumerate(self.separations_to_make):
+            if abs(line_coefficients[0] * self.circle_center[0] + line_coefficients[1] * self.circle_center[1] + line_coefficients[2]) / sqrt(line_coefficients[0] ** 2 + line_coefficients[1] ** 2) >= self.circle_radius:
+                continue
+            new_object_data_points = []
+            for j, point in enumerate(list(self.points)):
+                if abs(line_coefficients[0] * point.x + line_coefficients[1] * point.y + line_coefficients[2]) / sqrt(line_coefficients[0] ** 2 + line_coefficients[1] ** 2) < 0:
+                    self.points.pop(j)
+                    new_object_data_points.append(point)
+            self.circle = fit_circle_to_points([point.x, point.y] for point in self.points)
+            new_object_data = ObjectData()
+            new_object_data.points = new_object_data_points
+            new_object_data.separations_to_make = line_coefficients[i+1:]
+            new_splits.append(new_object_data)
+            new_splits.extend(new_object_data.commit())
+        return new_splits
+
+class DetectionService(Thread):
+    def __init__(self, lidar_service: LidarService):
+        super().__init__()
+        self.objects = [ObjectData()]
+        self.lidar_service = lidar_service
+
+    def run(self):
+        while True:
+            points = self.lidar_service.get_values()
+            for obj in self.objects:
+                obj.points.clear()
+            for point in points:
+                if point.x < 0 or point.x > 3000 or point.y < 0 or point.y > 2000:
+                    for obj in list(self.objects):
+                        x_coef = cos(point.absolute_angle)
+                        y_coef = sin(point.absolute_angle)
+                        constant = -x_coef * point.x - y_coef * point.y
+                        obj.separations_to_make.append((x_coef, y_coef, constant))
+                else:
+                    minimal_distance = inf
+                    object_with_minimal_distance = None
+                    for obj in self.objects:
+                        distance_to_circle = np.linalg.norm(point - obj.circle_center) - obj.circle_radius
+                        if distance_to_circle < 0:
+                            object_with_minimal_distance = obj
+                            break
+                        if distance_to_circle < minimal_distance:
+                            minimal_distance = distance_to_circle
+                            object_with_minimal_distance = obj
+                    object_with_minimal_distance.points.append(point)
+            objects_updated = []
+            for object in self.objects:
+                objects_updated.extend(object.commit())
+                if len(object.points) != 0:
+                    objects_updated.append(object)
+            if len(objects_updated) == 0:
+                objects_updated = [ObjectData()]
+            self.objects = objects_updated
+
 def stick_bytes(bytesList):
-        output = 0
-        for i in range(len(bytesList)):
-            output |= bytesList[i] << (8*i)
-        return output
-        
+    output = 0
+    for i in range(len(bytesList)):
+        output |= bytesList[i] << (8*i)
+    return output
+
+# From ChatGPT
+
+def dist_point_to_circle(point, center, radius):
+    """
+    Calculate the distance between a point and a circle's boundary.
+    """
+    return np.abs(np.linalg.norm(point - center) - radius)
+
+def objective_function(params, points):
+    """
+    Objective function for least squares circle fitting.
+    """
+    center = params[:2]
+    radius = params[2]
+    distances = np.array([dist_point_to_circle(point, center, radius) for point in points])
+    return np.sum(distances ** 2)
+
+def fit_circle_to_points(points):
+    """
+    Fit a circle to a set of points using least squares circle fitting.
+    """
+    # Initial guess for the center and radius
+    initial_guess = np.mean(points, axis=0).tolist() + [np.max(np.linalg.norm(points - np.mean(points, axis=0)))]
+    
+    # Minimize the objective function
+    result = minimize(objective_function, initial_guess, args=(points,), method='Nelder-Mead')
+    
+    # Extract the optimized parameters
+    center = result.x[:2]
+    radius = result.x[2]
+    return center, radius
+
 
 if __name__ == "__main__":
     thread = LidarService()
