21092edfb43d1d19d93c98eb06e4b84d6fbb1753
Teddy Roncin <teddy.roncin@proton.me>
2024-05-04 19:10:51 +0200

    added i2c communication

--- master_tiik/game_manager.py
+++ master_tiik/game_manager.py
@@ -4,7 +4,8 @@ import time
 
 from communication import CommunicationService
 from master_tiik.utils import Line, Point
-from useful_class import StationArea, GardenArea, GardenPotArea, GameState, PotArea, Path, PlantArea, Area, Action
+from useful_class import StationArea, GardenArea, GardenPotArea, GameState, PotArea, Path, PlantArea, Area, Action, \
+    MoveAction
 from math import pi, atan, sqrt, atan2
 
 distance_tiik_pot = 14
@@ -67,7 +68,9 @@ class GameManager(Thread):
         self.define_areas()
         while True:
             for area, path in strat.find_best_strategy(self.game_state):
-                self.define_next_actions(area, path)
+                self.create_actions_for_area(area, path)
+            for action in self.actions:
+                self.send_next_action()
                 self.wait_end_action()
 
     def define_areas(self):
@@ -84,38 +87,21 @@ class GameManager(Thread):
             else :
                 print("Still no color")
 
-    def define_next_actions(self, element: Area, path: Path):
+    def create_actions_for_area(self, element: Area, path: Path):
         for point in path.points[:-1]:
-            self.actions.append((point[0], point[1], None))
+            self.actions.append(MoveAction(x=point[0], y=point[1]))
         last_point = Point(path.points[-2][0], path.points[-2][1])
         destination = Point(path.points[-1][0], path.points[-1][1])
         distance = destination - last_point
         destination = destination + (destination - last_point) / distance * element.radius
-        self.actions.append((destination.x, destination.y, atan2(destination.y - last_point.y, destination.x - last_point.x)))
-        if isinstance(element, PlantArea):
-            pass
-        elif isinstance(element, GardenArea):
-            pass
-        elif isinstance(element, PotArea):
-            pass
-        elif isinstance(element, StationArea):
-            pass
-        elif isinstance(element, Area):
-            pass
-        elif isinstance(element, GardenPotArea):
-            pass
-        elif isinstance(element, GardenPotArea):
-            pass
+        self.actions.append(MoveAction(x=destination.x, y=destination.y, angle=atan2(destination.y - last_point.y, destination.x - last_point.x)))
+        action = element.get_action()
+        if action is not None:
+            self.actions.append(action)
 
     def wait_end_action(self):
-        while self.action_running:
-            self.define_action_state()
+        while not self.com_service.is_action_done():
             time.sleep(0.1)
 
-    def define_action_state(self):
-        self.action_running = self.com_service.receive_data()
-
-    def sent_action(self):
-        self.com_service.send_data(self.actions.pop(0))
-        
-        
+    def send_next_action(self):
+        self.com_service.send_action(self.actions.pop(0))
